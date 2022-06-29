17876e32298d58e61d4fd8a2bf331e7babdc3562
Solodov Denis <d.v.solodov@yandex.ru>
2022-06-29 14:57:48 +1000

    feat: update routes/user.js

--- routes/user.js
+++ routes/user.js
@@ -1,30 +1,20 @@
 const express = require('express');
 const { celebrate, Joi } = require('celebrate');
 const {
-  getUsers,
   getUser,
   getUserById,
   updateUser,
-  updateAvatar,
 } = require('../controllers/users');
-const { urlPattern } = require('../utils/constants');
+const { emailPattern } = require('../utils/constants');
 
 const router = express.Router();
 
-router.get('/', getUsers);
-
 router.get('/me', getUser);
-
-router.patch('/me/avatar', celebrate({
-  body: Joi.object().keys({
-    avatar: Joi.string().pattern(urlPattern),
-  }),
-}), updateAvatar);
 
 router.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    about: Joi.string().min(2).max(30),
+    email: Joi.string().required().pattern(emailPattern),
   }),
 }), updateUser);
 
