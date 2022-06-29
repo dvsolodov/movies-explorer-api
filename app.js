b9c843f52e0819a194873505d5dfd9eb9b2d03a6
Solodov Denis <d.v.solodov@yandex.ru>
2022-06-29 13:18:43 +1000

    feat: update app.js

---
+++ app.js
@@ -0,0 +1,94 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { celebrate, Joi } = require('celebrate');
+
+const { errors } = require('celebrate');
+const NotFoundError = require('./errors/not-found-err');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const userRouter = require('./routes/users');
+const movieRouter = require('./routes/movies');
+
+const { login, createUser } = require('./controllers/users');
+const auth = require('./middlewares/auth');
+
+const { PORT = 3000 } = process.env;
+const { urlPattern, allowedCors } = require('./utils/constants');
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+
+    return res.end();
+  }
+
+  return next();
+});
+
+app.use(requestLogger);
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(urlPattern),
+  }),
+}), createUser);
+
+app.use(auth);
+app.use('/users', userRouter);
+app.use('/movies', moviesRouter);
+app.use('/', () => {
+  throw new NotFoundError('Нет данных');
+});
+
+app.use(errorLogger);
+
+app.use(errors());
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? `На сервере произошла ошибка: ${err.name} = ${err.message}`
+        : message,
+    });
+
+  return next();
+});
+
+app.listen(PORT);
