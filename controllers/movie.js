65bcb5c848010e0d08ffb1afa85fe69691b79fdf
Solodov Denis <d.v.solodov@yandex.ru>
2022-06-29 16:25:19 +1000

    feat: update movie controller

---
+++ controllers/movie.js
@@ -0,0 +1,63 @@
+const Movie = require('../models/movie');
+const NotFoundError = require('../errors/not-found-err');
+const BadRequestError = require('../errors/bad-request-err');
+const ForbiddenError = require('../errors/forbidden-err');
+
+const getMovies = (req, res, next) => {
+  Movie.find({})
+    .then((movies) => {
+      if (!movies) {
+        throw new NotFoundError('Нет данных');
+      }
+
+      res.send(movies);
+    })
+    .catch(next);
+};
+
+const createMovie = (req, res, next) => {
+  const {
+    country, director, duration, year, owner = req.user._id, description, image, trailer, nameRU, nameEN, thumbnail, movieId
+  } = req.body;
+
+  Movie.create({
+    country, director, duration, year, owner, description, image, trailer, nameRU, nameEN, thumbnail, movieId
+  })
+    .then((movie) => {
+      if (!movie) {
+        throw new NotFoundError('Нет данных');
+      }
+
+      res.send(movie);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return next(new BadRequestError('Для создания карточки переданы некорректные данные'));
+      }
+
+      return next(err);
+    });
+};
+
+
+const deleteMovie = (req, res, next) => {
+  const { movieId } = req.params;
+
+  Movie.findById(movieId)
+    .orFail(() => new NotFoundError('Карточка с таким ID не найдена'))
+    .then((movie) => {
+      if (!movie.owner.equals(req.user._id)) {
+        return next(new ForbiddenError('Нельзя удалять чужие карточки'));
+      }
+
+      return movie.remove()
+        .then(() => res.send({ message: 'Карточка удалена' }));
+    })
+    .catch(next);
+};
+
+module.exports = {
+  getMovies,
+  deleteMovie,
+  createMovie,
+};
