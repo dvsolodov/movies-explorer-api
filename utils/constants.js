9653470d6191e17e7dfc73ea1b7c19fc7dff902a
Solodov Denis <d.v.solodov@yandex.ru>
2022-06-29 19:34:24 +1000

    feat: add a regexp for movie year

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,7 @@
 const urlPattern = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b[-a-zA-Z0-9()@:%_.~#?&//=+]*/;
 const passwordPattern = /^[a-zA-Z0-9]{8,}$/;
 const emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
+const movieYear = /^\d{4}$/;
 const allowedCors = [
   'http://solodov.students.nomoredomains.xyz',
   'https://solodov.students.nomoredomains.xyz',
@@ -12,5 +12,6 @@ module.exports = {
   urlPattern,
   passwordPattern,
   emailPattern,
+  movieYear,
   allowedCors,
 };
