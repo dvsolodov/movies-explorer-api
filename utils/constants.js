dac58980e521d4ee258755d16154b3844beff7eb
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-02 20:15:54 +1000

    fix: add server url

--- utils/constants.js
+++ utils/constants.js
@@ -3,8 +3,8 @@ const passwordPattern = /^[a-zA-Z0-9]{8,}$/;
 const emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const movieYear = /^\d{4}$/;
 const allowedCors = [
-  'http://solodov.students.nomoredomains.xyz',
-  'https://solodov.students.nomoredomains.xyz',
+  'https://solodov-dplm.students.nomoredomains.xyz/',
+  'http://solodov-dplm.students.nomoredomains.xyz/',
   'http://localhost:3000',
 ];
 
