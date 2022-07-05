d487c44fa1946ebdd9486cdf88b3c488cbbe6970
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-05 12:30:02 +1000

    fix: delete field "owner" validation

--- middlewares/validators.js
+++ middlewares/validators.js
@@ -35,8 +35,6 @@ const createMovieValidator = celebrate({
     image: Joi.string().required().pattern(urlPattern),
     trailerLink: Joi.string().required().pattern(urlPattern),
     thumbnail: Joi.string().required().pattern(urlPattern),
-    owner: Joi.string().length(24).hex().optional()
-      .allow(''),
     movieId: Joi.string(),
     nameRU: Joi.string().required().min(2).max(255),
     nameEN: Joi.string().required().min(2).max(255),
