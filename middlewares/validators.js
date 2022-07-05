93dceba28dd2d5cb5fa0e5e43e3c2cca8876f293
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-05 14:23:43 +1000

    fix: do name parameter required in registerValidator

--- middlewares/validators.js
+++ middlewares/validators.js
@@ -10,11 +10,9 @@ const loginValidator = celebrate({
 
 const registerValidator = celebrate({
   body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(urlPattern),
   }),
 });
 
